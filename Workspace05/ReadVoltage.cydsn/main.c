38cb3944762acd4e81fc552d2578dd41a9953591
Dau Hong Quan <echcom1995@gmail.com>
2021-01-12 17:53:57 +0700

    anh review giup em voi

--- Workspace05/ReadVoltage.cydsn/main.c
+++ Workspace05/ReadVoltage.cydsn/main.c
@@ -20,7 +20,7 @@
 
 #define INTERRUPT_TIMER     (1.0/12000000.0) * Timer_1_TC_PERIOD_VALUE
 #define VOLTAGE_FACTOR     1.102586
-#define CURRENT_RATIO      0.0003787
+#define CURRENT_RATIO      0.125
 #define U_ADC               0
 #define N_ADC               1
 #define V_ADC               2
@@ -30,6 +30,8 @@
 #define NL_ADC              6
 #define I3_ADC              7
 
+
+uint8 channel_ADC[] = {U_ADC, N_ADC, V_ADC, W_ADC};
 uint32 timer_count = 0;
 
 uint32 over_step = 0;
@@ -77,7 +79,7 @@ void enable_read_freq(){
     enable_read_frequency = 1;
 }
 
-void complete_read_freq(){
+void wait_for_read_freq(){
     while (complete_read_frequency == 0){};
     enable_read_frequency = 0;
 }
@@ -91,149 +93,65 @@ void RMS_Current(float *current){
     *current = *current* CURRENT_RATIO;
 }
 
-void RMS_Voltage_Phase(float *rms_voltA, float *rms_voltB, float *rms_voltC, float *rms_gridA, float *rms_gridB, float *rms_gridC, float *rms_curA, float *pf){
-    int16 adc_volU[500], adc_volN[500], adc_volV[500], adc_volW[500], adc_volL1[500], adc_volL2[500], adc_volL3[500], adc_volNL[500];
-    int16  adc_curA[500];
-    int32 volU_sq_sum = 0, volV_sq_sum = 0, volW_sq_sum = 0, volN_sq_sum = 0, volL1_sq_sum = 0, volL2_sq_sum = 0, volL3_sq_sum = 0, volNL_sq_sum = 0;
-    int32  curA_sq_sum = 0;
-    int16 volU_offset = 0, volV_offset = 0, volW_offset = 0, volN_offset = 0, volL1_offset = 0, volL2_offset = 0, volL3_offset = 0, volNL_offset = 0;
-    int16 curA_offset = 0;
-    int32 volU_offset_sum = 0, volV_offset_sum = 0, volW_offset_sum = 0, volN_offset_sum = 0, volL1_offset_sum = 0, volL2_offset_sum = 0, volL3_offset_sum = 0 ,volNL_offset_sum = 0;
-    int32 curA_offset_sum = 0;
-    float rms_voltU = 0, rms_voltV = 0, rms_voltW = 0, rms_voltN = 0, rms_voltL1 = 0, rms_voltL2 = 0, rms_voltL3 = 0, rms_voltNL = 0;
+// đọc ADC giá trị điện áp 3 pha máy phát điện
+void Read_ADC_generator_voltage(int16 adc_vol[4][500], uint8 channel, uint16 count){
+    adc_vol[channel][count] = ADC_Read(channel);
+}
+
+// tính trung bình cộng giá trị offset
+void total_value_offset(int16 adc_vol[4][500], int32 vol_offset_sum[], uint16 count, uint8 len, int16 vol_offset[]){
+    for(uint8 i = 0; i < len; i++){
+        for(uint16 j = count-1; j >= 0 ; j--){
+            vol_offset_sum[i] += adc_vol[i][count];    
+        }
+        vol_offset[i] = vol_offset_sum[i]/count;
+    }
+}
+
+// tinh tích phân giá trị điện áp từng pha
+void read_integral_voltage(int16 adc_vol[4][500], int16 vol_offset[], uint16 count, uint8 len, int32 vol_sq_sum[], float rms_volt[]){
+    for(uint8 i = 0; i < len; i++){
+        for(uint16 j = 0; j < count; j++)
+        {
+            adc_vol[i][j] = adc_vol[i][j] - vol_offset[i];  
+            vol_sq_sum[i] = vol_sq_sum[i] + adc_vol[i][j] * adc_vol[i][j];
+        }
+     vol_sq_sum[i] = vol_sq_sum[i] / count; 
+     rms_volt[i] = sqrt(vol_sq_sum[i]);
+    }
+}
+
+void RMS_Voltage_Phase(float *rms_voltA, float *rms_voltB, float *rms_voltC){
+    int16 adc_vol[4][500]; //adc_volU, adc_volN, adc_volV, adc_volW
+    int32 vol_offset_sum[4];    //volU_offset_sum, volV_offset_sum, volW_offset_sum, volN_offset_sum, 
+    int16 vol_offset[4];    //  volU_offset = 0, volV_offset = 0, volW_offset = 0, volN_offset = 0
+    int32 vol_sq_sum[4];    //volU_sq_sum = 0, volV_sq_sum = 0, volW_sq_sum = 0, volN_sq_sum = 0
+    float rms_volt[4];      //rms_voltU = 0, rms_voltV = 0, rms_voltW = 0, rms_voltN = 0;
     uint16 count = 0;
+    
+    // lấy ADC trong 1 chu kỳ
     while(check_time_freq < tick_period){
-            adc_volU[count] = ADC_Read(U_ADC);
-            adc_volN[count] = ADC_Read(N_ADC);
-            adc_volV[count] = ADC_Read(V_ADC);
-            adc_volW[count] = ADC_Read(W_ADC);
-            
-            select_mux_pin(0, 0, 0);
-            adc_volL1[count] = ADC_Read(X1_ADC);
-            select_mux_pin(0, 0, 1);
-            adc_volL2[count] = ADC_Read(X1_ADC);
-            
-            adc_volL3[count] = ADC_Read(L3_ADC);
-            adc_volNL[count] = ADC_Read(NL_ADC);
-            adc_curA[count] = ADC_Read(I3_ADC);
-                
-            volU_offset_sum += adc_volU[count];
-            volN_offset_sum += adc_volN[count]; 
-            volV_offset_sum += adc_volV[count];
-            volW_offset_sum += adc_volW[count];
-            
-            volL1_offset_sum += adc_volL1[count];
-            volL2_offset_sum += adc_volL2[count];
-            volL3_offset_sum += adc_volL3[count];
-            volNL_offset_sum += adc_volNL[count];
-            
-            curA_offset_sum += adc_curA[count];
-            
-//            cur_offset_sum += adc_cur[count];
-            count++; 
+        for(uint8 i = 0; i < sizeof(channel_ADC); i++){
+            Read_ADC_generator_voltage(&adc_vol[i], channel_ADC[i], count);
+            }
+        count++; 
         }
         check_time_freq = 0;
         flag_adc_volt = 0;
-        volU_offset = volU_offset_sum/count;
-        volN_offset = volN_offset_sum/count;
-        volV_offset = volV_offset_sum/count;
-        volW_offset = volW_offset_sum/count;
-    
-        volL1_offset = volL1_offset_sum/count;
-        volL2_offset = volL2_offset_sum/count;
-        volL3_offset = volL3_offset_sum/count;
-        volNL_offset = volNL_offset_sum/count;
-        curA_offset = curA_offset_sum/count;
-       
-        for(int i=0;i<count;i++)
-        {
-            adc_volU[i] = adc_volU[i] - volU_offset;
-            adc_volN[i] = adc_volN[i] - volN_offset;
-            adc_volV[i] = adc_volV[i] - volV_offset;
-            adc_volW[i] = adc_volW[i] - volW_offset;
-            
-            adc_volL1[i] = adc_volL1[i] - volL1_offset;
-            adc_volL2[i] = adc_volL2[i] - volL2_offset;
-            adc_volL3[i] = adc_volL3[i] - volL3_offset;
-            adc_volNL[i] = adc_volNL[i] - volNL_offset;
-            adc_curA[i] = adc_curA[i] - curA_offset;
-            
-            volU_sq_sum = volU_sq_sum + adc_volU[i] * adc_volU[i];
-            volN_sq_sum = volN_sq_sum + adc_volN[i] * adc_volN[i];
-            volV_sq_sum = volV_sq_sum + adc_volV[i] * adc_volV[i];
-            volW_sq_sum = volW_sq_sum + adc_volW[i] * adc_volW[i];
-            
-            volL1_sq_sum = volL1_sq_sum + adc_volL1[i] * adc_volL1[i];
-            volL2_sq_sum = volL2_sq_sum + adc_volL2[i] * adc_volL2[i];
-            volL3_sq_sum = volL3_sq_sum + adc_volL3[i] * adc_volL3[i];
-            volNL_sq_sum = volNL_sq_sum + adc_volNL[i] * adc_volNL[i];
-            curA_sq_sum = curA_sq_sum + adc_curA[i] * adc_curA[i];
-            real_power = real_power + adc_volU[i] * adc_curA[i];
-        }
+        total_value_offset(adc_vol, vol_offset_sum, count, sizeof(channel_ADC), vol_offset);
+        read_integral_voltage(adc_vol, vol_offset, count, sizeof(channel_ADC), vol_sq_sum,  rms_volt);
                
-        /*VrmsU Calculation*/
-        volU_sq_sum = volU_sq_sum/count;
-        rms_voltU = sqrt(volU_sq_sum);
-        
-        /*VrmsN Calculation*/
-        volN_sq_sum = volN_sq_sum/count;
-        rms_voltN = sqrt(volN_sq_sum);
-        
-        /*VrmsV Calculation*/
-        volV_sq_sum = volV_sq_sum/count;
-        rms_voltV = sqrt(volV_sq_sum);
-        
-        /*VrmsW Calculation*/
-        volW_sq_sum = volW_sq_sum/count;
-        rms_voltW = sqrt(volW_sq_sum);
-        
-        /*VrmsL1 Calculation*/
-        volL1_sq_sum = volL1_sq_sum/count;
-        rms_voltL1 = sqrt(volL1_sq_sum);
-        
-        /*VrmsL2 Calculation*/
-        volL2_sq_sum = volL2_sq_sum/count;
-        rms_voltL2 = sqrt(volL2_sq_sum);
-        
-        /*VrmsL3 Calculation*/
-        volL3_sq_sum = volL3_sq_sum/count;
-        rms_voltL3 = sqrt(volL3_sq_sum);
-        
-        /*VrmsNL Calculation*/
-        volNL_sq_sum = volNL_sq_sum/count;
-        rms_voltNL = sqrt(volNL_sq_sum);
-        
-        /*RmsIA Calculation*/
-        curA_sq_sum = curA_sq_sum / count;
-        *rms_curA = sqrt(curA_sq_sum);
-        
-        /*Power Factor calculation*/
-        real_power = real_power / count;
-        *pf = real_power/(rms_voltU * (*rms_curA));
-        
         /*VrmsU_N Calculation*/
-        *rms_voltA = rms_voltU + rms_voltN;
+        *rms_voltA = rms_volt[0] + rms_volt[1];
         *rms_voltA = *rms_voltA * VOLTAGE_FACTOR;
         
         /*VrmsV_N Calculation*/
-        *rms_voltB = rms_voltV + rms_voltN;
+        *rms_voltB = rms_volt[2] + rms_volt[1];
         *rms_voltB = *rms_voltB * VOLTAGE_FACTOR;
         
         /*VrmsW_N Calculation*/
-        *rms_voltC = rms_voltW + rms_voltN;
+        *rms_voltC = rms_volt[3] + rms_volt[1];
         *rms_voltC = *rms_voltC * VOLTAGE_FACTOR;
-        
-        /*VrmsL1_NL Calculation*/
-        *rms_gridA = rms_voltL1 + rms_voltNL;
-        *rms_gridA = *rms_gridA * VOLTAGE_FACTOR;
-        
-        /*VrmsL2_NL Calculation*/
-        *rms_gridB = rms_voltL2 + rms_voltNL;
-        *rms_gridB = *rms_gridB * VOLTAGE_FACTOR;
-        
-        /*VrmsL3_NL Calculation*/
-        *rms_gridC = rms_voltL3 + rms_voltNL;
-        *rms_gridC = *rms_gridC * VOLTAGE_FACTOR;
 }
 
 //quandh
@@ -314,15 +232,11 @@ int main(void)
     for(;;)
     {
         enable_read_freq();
-        complete_read_freq();
+        wait_for_read_freq();
         my_printf("frequency %0d.%02d\r\n", (int16)frequency, ((int16)(frequency * 100))%100);
         start_read_adc();
-        RMS_Voltage_Phase(&rms_voltP1, &rms_voltP2, &rms_voltP3, &rms_voltG1, &rms_voltG2, &rms_voltG3, &rms_curPA, &power_factor);
+        RMS_Voltage_Phase(&rms_voltP1, &rms_voltP2, &rms_voltP3);
         RMS_Current(&rms_curPA);
-        /*Irms Calculation*/
-//        cur_sq_sum = cur_sq_sum/count;
-//        rms_cur = sqrt(cur_sq_sum);
-        
         
 //        rms_volt = ADC_CountsTo_mVolts(0,rms_volt);
         
@@ -356,10 +270,8 @@ int main(void)
 //            my_printf("adc_vol[i]: %d\r\n", adc_vol[i]);
 ////            my_printf("adc_cur[i]: %d\r\n", adc_cur[i]);
 //        }
-//        MUX_A_Write(1);
+        
         CyDelay(1000);
-//        MUX_A_Write(0);
-//        CyDelay(1000);
         
 //        my_printf("quan\r\n");
 //        CyDelay(1000);
